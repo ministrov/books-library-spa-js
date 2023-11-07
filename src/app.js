09e8cd81ed688a6e9149802e4a6e9c9b16dd6f1b
unknown <ministrov2018@gmail.com>
2023-11-07 12:50:59 +0300

    feat: create abstract class of view

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
+import { MainView } from "./views/main/main";
 
 
 class App {
   routes = [
-    {path: "", view: null}
+    {path: "", view: MainView}
   ];
 
   constructor() {
@@ -11,36 +12,52 @@ class App {
   }
 
   route() {
-
+    if (this.currentView) {
+      this.currentView.destroy();
+    }
+    const view = this.routes.find(route => route.path == location.hash).view;
+    this.currentView = new view();
+    this.currentView.render();
   }
 }
 
 new App();
 
-const a = [1];
+// const a = [1];
 
-console.dir(a);
+// console.dir(a);
 
-const User = function(email, password) {
-  this.email = email;
-  this.password = password;
-}
+// const User = function(email, password) {
+//   this.email = email;
+//   this.password = password;
+// }
 
-const user1 = new User('a@a.ru', '123');
+// const user1 = new User('a@a.ru', '123');
 
-console.log(user1);
-console.log(user1 instanceof User);
+// console.log(user1);
+// console.log(user1 instanceof User);
 
-const Book = function(title, author) {
-  this.title = title;
-  this.author = author;
-  this.isRead = false;
-}
+// const Book = function(title, author) {
+//   this.title = title;
+//   this.author = author;
+//   this.isRead = false;
+// }
 
-Book.prototype.read = function() {
-  this.isRead = true;
-}
+// Book.prototype.read = function() {
+//   this.isRead = true;
+// }
 
-const lordOfTheRing = new Book('Lord of the Ring', 'Tolkien');
-console.log(lordOfTheRing);
-console.log(lordOfTheRing.__proto__);
+// const lordOfTheRing = new Book('Lord of the Ring', 'Tolkien');
+// console.log(lordOfTheRing);
+// console.log(lordOfTheRing.__proto__);
+// console.log(lordOfTheRing instanceof Book);
+// console.log(Object.prototype.isPrototypeOf.call(Book.prototype, lordOfTheRing));
+// console.log(Object.prototype.hasOwnProperty.call(lordOfTheRing, 'author'));
+
+// const myDate = new Date();
+
+// console.log(myDate);
+// console.log(myDate.getDate());
+// console.log(myDate.getUTCDate());
+
+// console.log(myDate instanceof Date);
