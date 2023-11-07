7ca17363b0aba659895296042c3c7baf895f722d
unknown <ministrov2018@gmail.com>
2023-11-07 11:23:38 +0300

    feat: just operating with function construstor

--- src/app.js
+++ src/app.js
@@ -16,3 +16,31 @@ class App {
 }
 
 new App();
+
+const a = [1];
+
+console.dir(a);
+
+const User = function(email, password) {
+  this.email = email;
+  this.password = password;
+}
+
+const user1 = new User('a@a.ru', '123');
+
+console.log(user1);
+console.log(user1 instanceof User);
+
+const Book = function(title, author) {
+  this.title = title;
+  this.author = author;
+  this.isRead = false;
+}
+
+Book.prototype.read = function() {
+  this.isRead = true;
+}
+
+const lordOfTheRing = new Book('Lord of the Ring', 'Tolkien');
+console.log(lordOfTheRing);
+console.log(lordOfTheRing.__proto__);
