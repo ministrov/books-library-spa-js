c16b94ba89cfb2eb66d568f2bb90b8667cf89f03
unknown <ministrov2018@gmail.com>
2023-11-08 11:22:07 +0300

    feat: operating on header styling

--- dist/app.js
+++ dist/app.js
@@ -1017,6 +1017,48 @@
   onChange.target = proxy => (proxy && proxy[TARGET]) || proxy;
   onChange.unsubscribe = proxy => proxy[UNSUBSCRIBE] || proxy;
 
+  class DivComponent {
+    constructor() {
+      this.el = document.createElement('div');
+    }
+
+    render() {
+      this.el;
+    }
+  }
+
+  class Header extends DivComponent {
+    constructor(appState) {
+      super();
+      this.appState = appState;
+    }
+
+    render() {
+      this.el.innerHTML = '';
+      this.el.classList = 'header';
+      this.el.innerHTML = `
+      <div>
+        <img src="/static/logo.svg" alt="Логотип" />
+      </div>
+      <div class="menu">
+        <a class="menu__item" href="#">
+          <img src="/static/search.svg" alt="Поиск" />
+          Поиск книг
+        </a>
+        <a class="menu__item" href="#">
+          <img src="/static/favorites.svg" alt="Избранное" />
+          Избранное
+
+          <div class="menu__counter">
+            ${this.appState.favorites.length}
+          </div>
+        </a>
+      </div>
+    `;
+      return this.el;
+    }
+  }
+
   class MainView extends AbstractView {
     state = {
       list: [],
@@ -1040,10 +1082,14 @@
 
     render() {
       const main = document.createElement('div');
-      main.innerHTML = `Число книг: ${this.appState.favorites.length}`;
       this.app.innerHTML = '';
       this.app.append(main);
-      this.appState.favorites.push('t');
+      this.renderHeader();
+    }
+
+    renderHeader() {
+      const header = new Header(this.appState).render();
+      this.app.prepend(header);
     }
   }
 
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import { DivComponent } from "../../common/div-component";
+import './header.css';
 
 export class Header extends DivComponent {
   constructor(appState) {
@@ -11,8 +12,23 @@ export class Header extends DivComponent {
     this.el.classList = 'header';
     this.el.innerHTML = `
       <div>
-        <img src="" alt="Логотип"
+        <img src="/static/logo.svg" alt="Логотип" />
+      </div>
+      <div class="menu">
+        <a class="menu__item" href="#">
+          <img src="/static/search.svg" alt="Поиск" />
+          Поиск книг
+        </a>
+        <a class="menu__item" href="#">
+          <img src="/static/favorites.svg" alt="Избранное" />
+          Избранное
+
+          <div class="menu__counter">
+            ${this.appState.favorites.length}
+          </div>
+        </a>
       </div>
     `
+    return this.el;
   }
 }
