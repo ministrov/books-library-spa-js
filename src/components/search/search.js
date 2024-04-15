f66ec2273f6097ec2d693f54a07b6f6ce8505ce6
unknown <ministrov2018@gmail.com>
2024-04-15 15:32:17 +0300

    fix: fixing search component

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,5 +1,5 @@
 import { DivComponent } from "../../common/div-component";
-import './search.css';
+import "./search.css";
 
 export class Search extends DivComponent {
   constructor(state) {
@@ -8,19 +8,19 @@ export class Search extends DivComponent {
   }
 
   search() {
-    const value = this.el.querySelector('input').value;
+    const value = this.el.querySelector("input").value;
     this.state.searchQuery = value;
   }
 
   render() {
-    this.el.classList.add('search');
+    this.el.classList.add("search");
     this.el.innerHTML = `
       <div class="search__wrapper">
         <input 
           type="text" 
           placeholder="Найти книгу или автора...." 
           class="search__input" 
-          value="${this.state.searchQuery ? this.state.searchQuery : ''}"
+          value="${this.state.searchQuery ? this.state.searchQuery : ""}"
         />
         <img src="static/search.svg" alt="Иконка поиска" />
       </div>
@@ -29,12 +29,14 @@ export class Search extends DivComponent {
       </button>
     `;
 
-    this.el.querySelector('button').addEventListener('click', this.search.bind(this));
-    this.el.querySelector('input').addEventListener('keydown', (event) => {
-      if (event.code === 'Enter') {
+    this.el
+      .querySelector("button")
+      .addEventListener("click", this.search.bind(this));
+    this.el.querySelector("input").addEventListener("keydown", (event) => {
+      if (event.code === "Enter") {
         this.search();
       }
-    })
+    });
     return this.el;
   }
 }
